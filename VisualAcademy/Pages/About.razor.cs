505a7db946f1f9e1a8f9e3fef0c4095ebe26bc30
K <K@DESCKTOP>
2020-06-28 18:26:53 +0900

    Blazor 컴포넌트 구조 및 코드 비하인드 파일 생성 그리고 OnInitialized 이벤트 핸들러 처리기 추가

---
+++ VisualAcademy/Pages/About.razor.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+
+namespace VisualAcademy.Pages
+{
+    public partial class About : ComponentBase
+    {
+        private string title = "정보";
+
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+
+        private string subTitle = "사이트 정보";
+
+        public string SubTitle
+        {
+            get { return subTitle; }
+            set { subTitle = value; }
+        }
+
+        protected override void OnInitialized()
+        {
+            subTitle = DateTime.Now.ToLongTimeString();
+        }
+    }
+}
